7158408340b032fdaa3a7a24ad1c408a903921e3
Mel <larosa.melania@gmail.com>
2024-10-14 09:39:24 -0300

    Mejora de grafico

--- analisis.js
+++ analisis.js
@@ -14,36 +14,37 @@ const analisisSangre = [
     { fecha: '17-08-2018', tipoEstudio: 'TSH', resultado: 2.80},
     { fecha: '17-08-2018', tipoEstudio: 'Triglicéridos', resultado:68},
     { fecha: '17-08-2018', tipoEstudio: 'Glóbulos Rojos', resultado: 4.79},
+    { fecha: '17-08-2018', tipoEstudio: 'Colesterol', resultado: 174},
     { fecha: '17-08-2019', tipoEstudio: 'Vitamina D', resultado: 28.2},
     { fecha: '17-08-2019', tipoEstudio: 'Glucosa', resultado: 0.80},
     { fecha: '17-08-2019', tipoEstudio: 'TSH', resultado: 2.38},
     { fecha: '17-08-2019', tipoEstudio: 'Triglicéridos', resultado:98},
     { fecha: '17-08-2019', tipoEstudio: 'Glóbulos Rojos', resultado: 5.04},
-    { fecha: '17-08-2019', tipoEstudio: 'Colesterol', resultado: 144},
+    { fecha: '17-08-2019', tipoEstudio: 'Colesterol', resultado: 236},
     { fecha: '11-10-2022', tipoEstudio: 'Vitamina D', resultado: 29.2},
     { fecha: '11-10-2022', tipoEstudio: 'Glucosa', resultado: 0.83},
     { fecha: '11-10-2022', tipoEstudio: 'TSH', resultado: 1.81},
     { fecha: '11-10-2022', tipoEstudio: 'Triglicéridos', resultado: 85},
     { fecha: '11-10-2022', tipoEstudio: 'Glóbulos Rojos', resultado: 4.58},
-    { fecha: '11-10-2022', tipoEstudio: 'Colesterol', resultado: 141.0},
+    { fecha: '11-10-2022', tipoEstudio: 'Colesterol', resultado: 250.0},
     { fecha: '19-07-2023', tipoEstudio: 'Vitamina D', resultado: 32.3},
     { fecha: '19-07-2023', tipoEstudio: 'Glucosa', resultado: 0.86},
     { fecha: '19-07-2023', tipoEstudio: 'TSH', resultado: 4.207},
     { fecha: '19-07-2023', tipoEstudio: 'Triglicéridos', resultado: 41.00},
     { fecha: '19-07-2023', tipoEstudio: 'Glóbulos Rojos', resultado: 4.58},
-    { fecha: '19-07-2023', tipoEstudio: 'Colesterol', resultado: 157.0},
+    { fecha: '19-07-2023', tipoEstudio: 'Colesterol', resultado: 275.0},
     { fecha: '18-12-2023', tipoEstudio: 'Vitamina D', resultado: 31.0},
     { fecha: '18-12-2023', tipoEstudio: 'Glucosa', resultado: 0.90},
     { fecha: '18-12-2023', tipoEstudio: 'TSH', resultado: 2.791},
     { fecha: '18-12-2023', tipoEstudio: 'Triglicéridos', resultado: 66},
     { fecha: '18-12-2023', tipoEstudio: 'Glóbulos Rojos', resultado: 4.72},
-    { fecha: '18-12-2023', tipoEstudio: 'Colesterol', resultado: 150.0},
+    { fecha: '18-12-2023', tipoEstudio: 'Colesterol', resultado: 277.0},
     { fecha: '09-10-2024', tipoEstudio: 'Vitamina D', resultado: 30.00},
     { fecha: '09-10-2024', tipoEstudio: 'Glucosa', resultado: 0.83},
     { fecha: '09-10-2024', tipoEstudio: 'TSH', resultado: 3.05},
     { fecha: '09-10-2024', tipoEstudio: 'Triglicéridos', resultado: 59.0},
     { fecha: '09-10-2024', tipoEstudio: 'Glóbulos Rojos', resultado: 4.51},
-    { fecha: '09-10-2024', tipoEstudio: 'Colesterol', resultado: 158.0}
+    { fecha: '09-10-2024', tipoEstudio: 'Colesterol', resultado: 241.0}
 ];
 // Guardar el array en el LocalStorage, primero lo convierte a formato JSON y luego lo guarda
 localStorage.setItem('analisisSangre', JSON.stringify(analisisSangre));
@@ -100,7 +101,7 @@ function generarGrafico(labels, data, estadoResultados, tipoEstudio) {
                 label:`Resultado del estudio: ${tipoEstudio}${rangoTexto}`,
                 data: datosValidos,
                 backgroundColor: estadoResultados.map(estado => {
-                    if (estado === 'normal') return 'rgba(54, 162, 235, 0.2)'; // Azul para normal
+                    if (estado === 'normal') return '#61de7f'; // Azul para normal
                     if (estado === 'alto') return 'rgba(255, 99, 132, 0.2)';  // Rojo para alto
                     if (estado === 'bajo') return 'rgba(255, 206, 86, 0.2)';  // Amarillo para bajo
                 }),
